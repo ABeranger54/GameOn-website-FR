69275705f4e81f68766e46843e1271d7d80aa4ed
Alex2204 <Alex2204@ALEX2204-PC>
2022-09-13 06:06:05 +0200

    Affichage des erreurs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,7 +16,6 @@ const modalCloseButton = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseButton.addEventListener("click", closeModal);
-//document.querySelector("#email").addEventListener("click", debugMode)
 
 // launch modal form
 function launchModal() {
@@ -31,55 +30,44 @@ function validate(){
   const first = document.getElementById("first").value;
   const last = document.getElementById("last").value;
   const email = document.getElementById("email").value;
+  var emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
   const quantity = document.getElementById("quantity").value;
-  const cgu = document.getElementById("checkbox1").checked;
-  const location = document.querySelector('input[name="location"]:checked');
+
+  var fieldConditions = {
+    "first" : first.length >= 2,
+    "last" : last.length >= 2,
+    "email" : emailRegex.test(email),
+    "quantity" : !isNaN(quantity) && quantity != "" && quantity >= 0,
+    "location" : document.querySelector('input[name="location"]:checked'),
+    "cgu" : document.getElementById("checkbox1").checked
+  };
 
   var valid = true;
-  
-  if(first.length < 2){
-    showError("first");
-    valid = false;
-  }
 
-  if(last.length < 2){
-    showError("last");
-    valid = false;
-  }
-
-  var re = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-  if(!re.test(email)){
-    showError("email");
-    valid = false;
-  }
-
-  if(isNaN(quantity) || quantity == "" || quantity < 0){
-    showError("quantity");
-    valid = false;
-  }
-
-  if(!location){
-    showError("location");
-    valid = false;
-  }
-
-  if(!cgu){
-    showError("cgu");
-    valid = false;
+  for(var key in fieldConditions){
+    showError(fieldConditions[key], key);
+    if(!fieldConditions[key]){
+      valid = false;
+    }
   }
 
   return valid;
 }
 
-function showError(error){
-  document.getElementById("error-" + error).style.display = "block";
-  const field = document.querySelector('input[name="' + error + '"]');
-  if(field){
-    field.style.border = "1px solid red";
-  }
-  
-}
+function showError(valid, error){
 
-// function debugMode(){
-  
-// }
+  const errorDiv = document.getElementById("error-" + error);
+  const field = document.querySelector('input[name="' + error + '"]');
+
+  if(!valid){
+    errorDiv.style.display = "block";
+    if(field){
+      field.style.border = "1px solid red";
+    }
+  }else{
+    errorDiv.style.display = "none";
+    if(field){
+      field.style.border = "none";
+    }
+  }
+}
