774521c30e0b615f8c71627a9b916477fd1a6bdf
Alex2204 <Alex2204@ALEX2204-PC>
2022-09-13 03:16:07 +0200

    Vérification des champs du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,6 +16,7 @@ const modalCloseButton = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseButton.addEventListener("click", closeModal);
+//document.querySelector("#email").addEventListener("click", debugMode)
 
 // launch modal form
 function launchModal() {
@@ -26,4 +27,25 @@ function closeModal(){
   modalbg.style.display = "none";
 }
 
+function validate(){
+  const first = document.getElementById("first").value;
+  const last = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const quantity = document.getElementById("quantity").value;
+  const cgu = document.getElementById("checkbox1").checked;
+  const radio = document.querySelector('input[name="location"]:checked');
+  
+  if(first.length < 2) return false;
+  if(last.length < 2) return false;
+  var re = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+  if(!re.test(email)) return false;
+  if(isNaN(quantity) || quantity < 0) return false;
+  if(!radio) return false;
+  if(!cgu) return false;
 
+  return true;
+}
+
+// function debugMode(){
+  
+// }
